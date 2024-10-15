5467f0638357f4e9029f53c278c7af643b354d9a
Sandeep Yadav <sandeep@zeptodigitallabs.com>
2024-10-15 14:15:50 +0530

    1. android core api sdk release 2 android checkout sdk release 3. android sample app release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,21 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("java-library")
-    alias(libs.plugins.jetbrainsKotlinJvm)
+    id("maven-publish")
+    kotlin("jvm") version "1.9.0"
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+
+tasks.register<Wrapper>("wrapper") {
+    gradleVersion = "8.10.2"
+}
+tasks.register("prepareKotlinBuildScriptModel"){}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies{
@@ -13,6 +23,51 @@ dependencies{
     implementation (libs.kotlinx.coroutines.android)
     implementation (libs.retrofit)
     implementation (libs.converter.gson)
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation("org.json:json:20240303")
+    implementation (libs.okhttp3.logging.interceptor)
+    implementation(libs.json)
+}
+publishing {
+    val sdkArtifactId = "nimbbl-checkout-core-sdk-java"
+    val sdkGroupId = "tech.nimbbl.sdk"
+    val gitlabToken = "glpat-zswGgsyUM5yVbo9yy6RG"
+    val sdkVersion = "3.0.0"
+    val mavenRepo = "https://gitlab.com/api/v4/projects/25847308/packages/maven"
+
+    publications {
+        create<MavenPublication>("jarArchive") {
+            groupId = sdkGroupId
+            artifactId = sdkArtifactId
+            version = sdkVersion
+
+            // Specify the JAR artifact
+            from(components["java"])
+
+            // Generate valid POM
+            pom {
+                name.set(sdkArtifactId)
+                description.set("Nimbbl Checkout Core SDK for Java")
+                url.set("https://your-repository-url.com") // Set a valid project URL
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri(mavenRepo)
+            if (!mavenRepo.startsWith("file")) {
+                credentials(HttpHeaderCredentials::class) {
+                    name = "Private-Token"
+                    value = gitlabToken
+                }
+                authentication {
+                    create<HttpHeaderAuthentication>("header")
+                }
+            }
+        }
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
---
+++ nimbbl_core_release_sdk.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e -u
+
+
+THIS_DIR=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)
+MVN_REPO="https://gitlab.com/api/v4/projects/25847308/packages/maven"
+DEFAULT_SDK_VERSION="3.0.0"
+SDK_VERSION="3.0.0"
+
+echo "Releasing Nimbbl SDK ${SDK_VERSION}"
+
+echo "Building and publishing the Nimbbl Checkout SDK"
+echo "Using ${MVN_REPO} as the Maven repo"
+
+pushd ${THIS_DIR}/
+./gradlew clean
+./gradlew assemble
+./gradlew publish
+popd
+
+# Done!
+echo "Finished! Don't forget to push the tag and the Maven repo artifacts."
