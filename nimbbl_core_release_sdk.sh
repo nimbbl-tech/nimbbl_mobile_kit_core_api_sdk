3570bc873a3d68712886254a8ad45beaa045da0e
Sandeep Yadav <sandeep@zeptodigitallabs.com>
2025-01-08 13:00:27 +0530

    changing pp url handling exception

--- nimbbl_core_release_sdk.sh
+++ nimbbl_core_release_sdk.sh
@@ -5,8 +5,8 @@ set -e -u
 
 THIS_DIR=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)
 MVN_REPO="https://gitlab.com/api/v4/projects/25847308/packages/maven"
-DEFAULT_SDK_VERSION="3.0.2"
-SDK_VERSION="3.0.2"
+DEFAULT_SDK_VERSION="3.0.5"
+SDK_VERSION="3.0.5"
 
 echo "Releasing Nimbbl SDK ${SDK_VERSION}"
 
