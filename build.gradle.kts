69e337579235226679fb409043e2e135f202d93e
SanNimbbl <sandeep@nimbbl.biz>
2025-08-05 16:30:28 +0530

    updated jdk version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("com.android.library") version "8.11.1"
+    id("com.android.library")
     kotlin("android") version "1.9.0"
     id("maven-publish")
 }
@@ -67,16 +67,28 @@ tasks.withType<KotlinCompile> {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
-    implementation("com.squareup.retrofit2:retrofit:3.0.0")
-    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.1.0")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0") {
+        exclude(group = "com.sun.xml.bind", module = "jaxb-impl")
+        exclude(group = "javax.activation", module = "javax.activation-api")
+    }
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0") {
+        exclude(group = "com.sun.xml.bind", module = "jaxb-impl")
+        exclude(group = "javax.activation", module = "javax.activation-api")
+    }
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0") {
+        exclude(group = "com.sun.xml.bind", module = "jaxb-impl")
+        exclude(group = "javax.activation", module = "javax.activation-api")
+    }
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.3.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    implementation("com.auth0.android:jwtdecode:2.0.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.auth0.android:jwtdecode:2.0.0") {
+        exclude(group = "com.sun.xml.bind", module = "jaxb-impl")
+        exclude(group = "javax.activation", module = "javax.activation-api")
+    }
 }
 
 // Optional: include sources in the published artifact
